cfd1fe823d7697cdde09ff1ac99146985a517675
Dan Buchner <dan.buchner@gmail.com>
2018-03-12 19:35:27 -0400

    Removing some hardcode and adding a properties file.

--- src/main/java/com/danbuchner/contactform/controller/ContactFormController.java
+++ src/main/java/com/danbuchner/contactform/controller/ContactFormController.java
@@ -1,11 +1,10 @@
 package com.danbuchner.contactform.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-
 import com.danbuchner.contactform.email.EmailService;
 
 @Controller
@@ -15,17 +14,18 @@ public class ContactFormController {
 	@Autowired
 	EmailService emailService;
 	
+	@Value("${emailSender.to}")
+	String emailTo;	
 	
 	@RequestMapping(value="/sendEmail")
 	public String sendEmail(@RequestParam("name") String contactName,
 			@RequestParam("email") String contactEmail,
-			@RequestParam("message") String contactMsg) {
+			@RequestParam("message") String contactMsg) {		
 		if((contactEmail!= null && contactEmail.length()>0)
 				&& (contactName!= null && contactName.length()>0)) {	
-			emailService.sendEmail("dan.buchner@gmail.com", contactEmail,"Contact from website", contactMsg);
+			emailService.sendEmail(emailTo, contactEmail,"Contact from website", contactMsg);
 			System.out.println("Email Sent");
 		}
 		return "index";
-	}
-	
+	}	
 }
